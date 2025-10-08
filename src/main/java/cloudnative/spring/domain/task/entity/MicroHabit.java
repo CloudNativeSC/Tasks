0e9378d9748c9825f1aa92e443a2cea62ae9a9e7
이지연 <writemumu@naver.com>
2025-10-09 01:36:33 +0900

    feat:모드에 사용하는 MicroHabit entity 추가

--- src/main/java/cloudnative/spring/domain/task/entity/MicroHabit.java
+++ src/main/java/cloudnative/spring/domain/task/entity/MicroHabit.java
@@ -6,9 +6,13 @@ import lombok.Builder;
 
 import java.time.LocalDateTime;
 import cloudnative.spring.domain.task.enums.HabitType;
+import lombok.Getter;
+import lombok.Setter;
 
 @Builder
 @Entity
+@Getter
+@Setter
 @Table(name = "micro_habits")
 public class MicroHabit extends BaseTimeEntity {
 
@@ -35,4 +39,14 @@ public class MicroHabit extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "habit_type")
     private HabitType habitType;  // PREDEFINED, CUSTOM
+
+    /**
+     * 습관 완료 처리
+     * - 완료 횟수 증가
+     * - 마지막 완료 시간 갱신
+     */
+    public void incrementCompletion() {
+        this.completionCount++;
+        this.lastCompletedAt = LocalDateTime.now();
+    }
 }
