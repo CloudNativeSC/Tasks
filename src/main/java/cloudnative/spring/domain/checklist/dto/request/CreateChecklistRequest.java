51d2b8f60d88b97b48919c6aa73b323332cd006b
이지연 <writemumu@naver.com>
2025-10-16 01:38:47 +0900

    feat:체크리스트 모드 DTO CreateChecklistRequest추가

---
+++ src/main/java/cloudnative/spring/domain/checklist/dto/request/CreateChecklistRequest.java
@@ -0,0 +1,10 @@
+package cloudnative.spring.domain.checklist.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreateChecklistRequest {
+    private String name;
+}
